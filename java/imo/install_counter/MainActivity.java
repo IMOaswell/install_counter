01877371c56edb47c999eb7c707695dcafa47b6c
IMOitself <russelljonnbautista@gmail.com>
2024-05-19 07:29:16 +0800

    read the file instead of sharedprefs:D

--- java/imo/install_counter/MainActivity.java
+++ java/imo/install_counter/MainActivity.java
@@ -2,76 +2,86 @@ package imo.install_counter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 
 public class MainActivity extends Activity 
 {
+    final File compiled_to_apk_count = new File("/storage/emulated/0/AppProjects/frog/compiled_to_apk_count.txt");
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         Intent intent = getIntent();
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             final Uri apkUri = intent.getData();
-            
-            final SharedPreferences sp = getSharedPreferences("uwu", MODE_PRIVATE);
-            final int i = sp.getInt("wawa", 0);
+            String savedCountInFile = FileTools.readTextFile(compiled_to_apk_count);
+            final int i = Integer.parseInt(savedCountInFile);
             final Button btn = findViewById(R.id.btn);
-//            sp.edit().putInt("wawa", 0).apply();//reset
-//            btn.setText(0+"");
-//            if(true) return;
-            btn.setText(i+"");
+            btn.setText(i + "");
             btn.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v){
-                        if (apkUri == null){
+                    public void onClick (View v) {
+                        if (apkUri == null) {
                             finishAffinity();
                             return;
                         }
                         btn.setEnabled(false);
                         int j = i + 1;
-                        writeIntToFile(j, "/storage/emulated/0/AppProjects/frog/compiled_to_apk_count.txt");
-                        btn.setText(j+"");
+                        FileTools.writeTextFile(compiled_to_apk_count, j + "");
+                        btn.setText(j + "");
                         installApk(apkUri);
-                        sp.edit().putInt("wawa", j).apply();
                     }
                 });
         }
     }
 
-    private void installApk(Uri apkUri) {
+    private void installApk (Uri apkUri) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         startActivity(intent);
     }
-    private void writeIntToFile(int value, String path) {
-        File file = new File(path);
-        
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
+
+    static class FileTools
+    {
+        static String readTextFile (File file) {
+            String filePath = file.getAbsolutePath();
+            return readTextFile(filePath);
+        }
+
+        static String readTextFile (String filePath) {
+            StringBuilder content = new StringBuilder();
+
             try {
-                fos.write(Integer.toString(value).getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (fos != null) {
-                    try {
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
                 }
-            }
-        } catch (FileNotFoundException e) {}
-        
+                reader.close();
+            } catch (IOException e) {}
+            return content.toString().trim();
+        }
+
+        static void writeTextFile (File file, String input) {
+            try {
+                if (!file.exists()) file.createNewFile();
+                if (file.isFile()) {
+                    FileWriter fw = new FileWriter(file);
+                    fw.write(input);
+                    fw.close();
+                }
+            } catch (IOException e) {}
+        }
     }
 }
