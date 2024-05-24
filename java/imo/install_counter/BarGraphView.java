76ce40a212c2b67aeff5c818e2d15c92ef21b458
IMOitself <russelljonnbautista@gmail.com>
2024-05-25 07:32:17 +0800

    add BarGraphView:D

---
+++ java/imo/install_counter/BarGraphView.java
@@ -0,0 +1,18 @@
+package imo.install_counter;
+
+import android.view.View;
+import android.content.Context;
+import android.graphics.Canvas;
+
+public class BarGraphView extends View
+ {
+    
+    BarGraphView(Context mContext){
+        super(mContext);
+    }
+
+    @Override
+    protected void onDraw (Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
