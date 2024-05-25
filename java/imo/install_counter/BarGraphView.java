48187bcde65aad639c6a592be6bf8340425d2bfd
IMOitself <russelljonnbautista@gmail.com>
2024-05-26 06:27:10 +0800

    move drawing of canvas to a method:D

--- java/imo/install_counter/BarGraphView.java
+++ java/imo/install_counter/BarGraphView.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
@@ -19,26 +20,25 @@ public class BarGraphView
                 public void onGlobalLayout () {
                     int padding = (int) layout.getWidth() / 60;
                     layout.setPadding(padding, padding, padding, padding);
-                    int width = layout.getWidth();
-                    int height = layout.getHeight();
-                    Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
-                    Canvas canvas = new Canvas(bitmap);
-                    Paint paint = new Paint();
-                    paint.setColor(Color.BLACK);
-                    paint.setStyle(Paint.Style.FILL);
-
-                    int canvasHeight = canvas.getHeight();
-                    int canvasWidth = canvas.getWidth();
-                    float[] line1 = {padding, padding, padding, canvasHeight - padding};
-                    canvas.drawLine(line1[0], line1[1], line1[2], line1[3], paint);
-                    float[] line2 = {padding, canvasHeight - padding, canvasWidth - padding, canvasHeight - padding};
-                    canvas.drawLine(line2[0], line2[1], line2[2], line2[3], paint);
-
-                    BitmapDrawable bitmapDrawable = new BitmapDrawable(layout.getResources(), bitmap);
-                    layout.setBackground(bitmapDrawable);
+                    layout.setBackground(drawCanvas(layout, layout.getWidth(), layout.getHeight(), padding));
                 }
             });
         return layout;
+    }
+    
+    static BitmapDrawable drawCanvas(View view, int width, int height, int padding){
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+
+        int canvasHeight = canvas.getHeight();
+        int canvasWidth = canvas.getWidth();
+        float[] line1 = {padding, padding, padding, canvasHeight - padding};
+        canvas.drawLine(line1[0], line1[1], line1[2], line1[3], paint);
+        float[] line2 = {padding, canvasHeight - padding, canvasWidth - padding, canvasHeight - padding};
+        canvas.drawLine(line2[0], line2[1], line2[2], line2[3], paint);
+        return new BitmapDrawable(view.getResources(), bitmap);
     }
 }
