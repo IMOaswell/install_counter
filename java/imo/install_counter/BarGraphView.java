4a64811ce3a0b9efca4667bec12f6f134a52e37b
IMOitself <russelljonnbautista@gmail.com>
2024-05-26 07:01:46 +0800

    add spacing to bars via margin:D

--- java/imo/install_counter/BarGraphView.java
+++ java/imo/install_counter/BarGraphView.java
@@ -16,25 +16,25 @@ public class BarGraphView
 {
     static final int BAR_SPACING = 25;
     static ViewGroup create (final Context mContext) {
-        
+
         final LinearLayout layout = new LinearLayout(mContext);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
                 public void onGlobalLayout () {
                     layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                    
+
                     int padding = (int) layout.getWidth() / 60;
-                    layout.setPadding(padding, padding, padding, padding);
+                    layout.setPadding(padding, padding, padding, padding + BAR_SPACING);
                     layout.setBackground(drawCanvas(layout, layout.getWidth(), layout.getHeight(), padding));
                 }
             });
-        
+
         layout.addView(createBar(mContext, "hello"));
         layout.addView(createBar(mContext, "world"));
         return layout;
     }
-    
+
     static BitmapDrawable drawCanvas(View view, int width, int height, int padding){
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
@@ -50,12 +50,17 @@ public class BarGraphView
         canvas.drawLine(line2[0], line2[1], line2[2], line2[3], paint);
         return new BitmapDrawable(view.getResources(), bitmap);
     }
-    
+
     static TextView createBar(Context mContext, String text){
         TextView textview = new TextView(mContext);
         textview.setText(text);
         textview.setBackgroundColor(Color.BLACK);
         textview.setTextColor(Color.WHITE);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT, 
+            LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, BAR_SPACING, 0, 0);
+        textview.setLayoutParams(layoutParams);
         return textview;
     }
 }
